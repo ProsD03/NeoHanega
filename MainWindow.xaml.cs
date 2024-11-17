a784d172abd7ee049fe9876fb2e63c8b7e8e44da
ProsD <alex.prosdocimo@gmail.com>
2024-11-17 20:48:09 +0100

    Started Implementation of NeoMusou
    Started implementation of the NeoMusou child application, that will be responsible to apply the necessary system changes to avoid the "Run as Admin" prompts.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -50,10 +50,10 @@ namespace NeoHanega
         private void uac_checkbox_Checked(object sender, RoutedEventArgs e)
         {
             String title = "Warning: Irreversable Action";
-            String message = "Hanega will make changes to certain Windows Registry keys and configuration files to remove " +
+            String message = "NeoHanega will make changes to certain Windows Registry keys and configuration files to remove " +
                 "the \"Run as Admin\" pop-up (UAC) requirements from the 3DMigoto and Genshin Impact executables. This action " +
                 "will only alter settings related to 3DMigoto and Genshin Impact. Nevertheless, this action may impact system " +
-                "functionality, and previous settings cannot be restored by Hanega.\n\nWould you like to proceed?";
+                "functionality, and previous settings cannot be restored by NeoHanega.\n\nWould you like to proceed?";
             MessageBoxButton buttons = MessageBoxButton.YesNo;
             MessageBoxImage icon = MessageBoxImage.Warning;
             
@@ -67,7 +67,18 @@ namespace NeoHanega
 
         private void ok_button_Click(object sender, RoutedEventArgs e)
         {
+            if (removeUAC)
+            {
+                String title = "Info";
+                String message = "To prevent the \"Run as Admin\" prompt when launching games, NeoHanega will now start a helper " +
+                        "program, Musou, which requires admin privileges to make the necessary system changes. You " +
+                        "will see a \"Run as Admin\" prompt for Musou, allowing it to apply these changes. After this " +
+                        "setup, you won’t see the prompt when starting your games.";
+                MessageBoxButton buttons = MessageBoxButton.OK;
+                MessageBoxImage icon = MessageBoxImage.Information;
 
+                MessageBox.Show(message, title, buttons, icon, MessageBoxResult.OK);
+            }
         }
 
         private void cancel_button_Click(object sender, RoutedEventArgs e)
